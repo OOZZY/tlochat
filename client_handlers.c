6a12b50ed158928270f6edaa591592c6438d058f
oozzy <oozzy.tlo@gmail.com>
2016-12-01 11:29:27 -0800

    Add condition variable synchronization to client handlers
    Also, Rename CLIENT_HANDLED to CLIENT_CLOSED.

--- client_handlers.c
+++ client_handlers.c
@@ -13,7 +13,7 @@
 typedef enum ClientState {
   CLIENT_UNHANDLED,
   CLIENT_BEING_HANDLED,
-  CLIENT_HANDLED
+  CLIENT_CLOSED
 } ClientState;
 
 typedef struct Client {
@@ -52,7 +52,11 @@ static const TloType clientPtrType = {.sizeOf = sizeof(Client *),
                                       .destruct = clientPtrDestruct};
 
 static TloDArray clientPtrs;
+static int numUnhandledClients = 0;
+static int numClosedClients = 0;
 static pthread_mutex_t clientsMutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t clientsUnhandled = PTHREAD_COND_INITIALIZER;
+pthread_cond_t clientsClosed = PTHREAD_COND_INITIALIZER;
 
 #define NUM_CLIENT_HANDLER_PTHREADS 4
 static pthread_t clientHandlers[NUM_CLIENT_HANDLER_PTHREADS];
@@ -67,11 +71,17 @@ static void *handleClients(void *data) {
     Client *clientPtr = NULL;
 
     pthread_mutex_lock(&clientsMutex);
+    while (continueHandling && numUnhandledClients == 0) {
+      errno = pthread_cond_wait(&clientsUnhandled, &clientsMutex);
+      assert(!errno);
+    }
+
     for (size_t i = 0; i < tloDArrayGetSize(&clientPtrs); ++i) {
       Client **clientPtrPtr = tloDArrayGetMutableElement(&clientPtrs, i);
       if ((*clientPtrPtr)->state == CLIENT_UNHANDLED) {
         (*clientPtrPtr)->state = CLIENT_BEING_HANDLED;
         clientPtr = *clientPtrPtr;
+        numUnhandledClients--;
         break;
       }
     }
@@ -110,11 +120,23 @@ static void *handleClients(void *data) {
       close(clientPtr->fd);
       printf("tlochat client handlers: closed connection from %s:%u\n",
              clientPtr->addressString, clientPtr->port);
-      clientPtr->state = CLIENT_HANDLED;
+      clientPtr->state = CLIENT_CLOSED;
+
+      pthread_mutex_lock(&clientsMutex);
+      numClosedClients++;
+      errno = pthread_cond_signal(&clientsClosed);
+      assert(!errno);
+      pthread_mutex_unlock(&clientsMutex);
     } else {
       // printf("tlochat client handlers: defer connection from %s:%u\n",
       // clientPtr->addressString, clientPtr->port);
       clientPtr->state = CLIENT_UNHANDLED;
+
+      pthread_mutex_lock(&clientsMutex);
+      numUnhandledClients++;
+      errno = pthread_cond_signal(&clientsUnhandled);
+      assert(!errno);
+      pthread_mutex_unlock(&clientsMutex);
     }
   }
 
@@ -147,14 +169,20 @@ static void *cleanClients(void *data) {
     nanosleep(&interval, IGNORE_OUT_ARG);
 
     pthread_mutex_lock(&clientsMutex);
+    while (continueHandling && numClosedClients == 0) {
+      errno = pthread_cond_wait(&clientsClosed, &clientsMutex);
+      assert(!errno);
+    }
+
     printf("tlochat client handlers: %zu clients\n",
            tloDArrayGetSize(&clientPtrs));
     printf("tlochat client handlers: cleaning up clients\n");
     for (size_t i = 0; i < tloDArrayGetSize(&clientPtrs); ++i) {
       Client **clientPtrPtr = tloDArrayGetMutableElement(&clientPtrs, i);
-      if ((*clientPtrPtr)->state == CLIENT_HANDLED) {
+      if ((*clientPtrPtr)->state == CLIENT_CLOSED) {
         tloDArrayUnorderedRemove(&clientPtrs, i);
         i--;
+        numClosedClients--;
       }
     }
     printf("tlochat client handlers: %zu clients\n",
@@ -213,6 +241,11 @@ int clientHandlersAddClient(int fd, const char *addressString, in_port_t port) {
 
   pthread_mutex_lock(&clientsMutex);
   TloError tloError = tloDArrayPushBack(&clientPtrs, &client);
+  if (!tloError) {
+    numUnhandledClients++;
+    errno = pthread_cond_signal(&clientsUnhandled);
+    assert(!errno);
+  }
   pthread_mutex_unlock(&clientsMutex);
 
   if (tloError) {
@@ -237,6 +270,13 @@ static void joinPthreads(pthread_t *pthreads, int numPthreads) {
 void clientHandlersCleanup() {
   printf("tlochat client handlers: cleaning up client handlers\n");
   continueHandling = false;
+
+  // stop all threads from waiting
+  errno = pthread_cond_broadcast(&clientsUnhandled);
+  assert(!errno);
+  errno = pthread_cond_broadcast(&clientsClosed);
+  assert(!errno);
+
   joinPthreads(&clientCleaner, NUM_CLIENT_CLEANER_PTHREADS);
   joinPthreads(clientHandlers, NUM_CLIENT_HANDLER_PTHREADS);
   tloDArrayDestruct(&clientPtrs);
